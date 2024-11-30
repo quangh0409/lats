6dbce28aa38f3d3cb9c24f0d6526db98d2b3bc17
quangh0409 <trongquangvu80@gmail.com>
2024-11-30 17:51:47 +0700

    Fix: cors

--- src/main/java/com/example/lats/LatsApplication.java
+++ src/main/java/com/example/lats/LatsApplication.java
@@ -4,11 +4,13 @@ import io.hypersistence.utils.spring.repository.BaseJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.example.lats"})
+@SpringBootApplication(scanBasePackages = {"com.example.lats"}, exclude = {
+		SecurityAutoConfiguration.class})
 @EntityScan("com.example.lats.model.entity")
 @EnableJpaRepositories(value = "com.example.lats.repository",
 		repositoryBaseClass = BaseJpaRepositoryImpl.class)
--- src/main/java/com/example/lats/common/cors/CorsConfig.java
+++ src/main/java/com/example/lats/common/cors/CorsConfig.java
@@ -2,23 +2,20 @@ package com.example.lats.common.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class CorsConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // Allow all paths
-                        .allowedOrigins("http://localhost:3000")  // Ensure this matches the front-end origin
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Ensure methods are correct
-                        .allowedHeaders("*")  // Allow all headers
-                        .allowCredentials(true);  // Allow credentials (e.g., cookies)
-            }
-        };
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults()) //enables cors
+                //The rest of your configurations
+                .build();
     }
 }
 
