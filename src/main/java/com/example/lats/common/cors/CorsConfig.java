a0537c69ca94d695b954032ac57960af6e2a807f
quangh0409 <trongquangvu80@gmail.com>
2024-11-30 16:41:13 +0700

    Fix: áºError Cá»ORS

--- src/main/java/com/example/lats/common/cors/CorsConfig.java
+++ src/main/java/com/example/lats/common/cors/CorsConfig.java
@@ -3,19 +3,17 @@ package com.example.lats.common.cors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all paths
-                        .allowedOrigins("*") // Allow specific origin(s)
+                        .allowedOrigins("http://127.0.01:3000") // Allow specific origin(s)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
