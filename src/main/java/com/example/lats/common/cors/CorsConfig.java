0df5bad549b27882d0d5dbb75e0a3a233c674c08
quangh0409 <trongquangvu80@gmail.com>
2024-11-30 17:56:36 +0700

    Fix: cors

--- src/main/java/com/example/lats/common/cors/CorsConfig.java
+++ src/main/java/com/example/lats/common/cors/CorsConfig.java
@@ -2,20 +2,23 @@ package com.example.lats.common.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
 public class CorsConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .cors(Customizer.withDefaults()) //enables cors
-                //The rest of your configurations
-                .build();
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Allow all paths
+                        .allowedOrigins("http://localhost:3000", "http://localhost:6868")  // Ensure this matches the front-end origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Ensure methods are correct
+                        .allowedHeaders("*")  // Allow all headers
+                        .allowCredentials(true);  // Allow credentials (e.g., cookies)un
+            }
+        };
     }
 }
 
